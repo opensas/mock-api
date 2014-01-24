b7b99eff8a6d89cc4fc0d2bb9acf590d21178614
opensas <opensas@gmail.com>
2014-01-24 04:12:44 -0300

    initial commit

---
+++ data/usuarios/data.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    id: 1,
+    nombre: 'juan',
+    apellido: 'mango',
+    edad: 22
+  },
+  {
+    id: 2,
+    nombre: 'abel',
+    apellido: 'sanchez',
+    edad: 24
+  },
+  {
+    id: 3,
+    nombre: 'ernesto',
+    apellido: 'gutierrez',
+    edad: 30
+  },
+];
