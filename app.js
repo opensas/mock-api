de94d5024f2675b242d3a212fdb4d775f3fc73a8
opensas <opensas@gmail.com>
2014-01-24 04:46:59 -0300

    added html test page

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.configure(function() {
-  // app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+  app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
   app.use(express.logger('dev'));                 // log every request to the console
   app.use(express.bodyParser());                  // pull information from html in POST
   app.use(express.methodOverride());              // simulate DELETE and PUT
