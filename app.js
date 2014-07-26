2cbb445432b1f446e626574aa909e7b39219b768
opensas <opensas@gmail.com>
2014-07-26 05:27:38 -0300

    fixes #5, upgrade to express 4

--- app.js
+++ app.js
@@ -4,15 +4,19 @@
 var express = require('express');
 var app     = express();
 
-// config
-var config      = require('./lib/utils/config');
+// middleware
+var serveStatic = require('serve-static')
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override')
 
-app.configure(function() {
-  app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
-  app.use(express.logger('dev'));                 // log every request to the console
-  app.use(express.bodyParser());                  // pull information from html in POST
-  app.use(express.methodOverride());              // simulate DELETE and PUT
-});
+// config
+var config = require('./lib/utils/config');
+
+app.use(serveStatic(__dirname + '/public'));    // set the static files location /public/img will be /img for users
+app.use(logger('dev'));                         // log every request to the console
+app.use(bodyParser.json());                     // pull information from html in POST
+app.use(methodOverride());                      // simulate DELETE and PUT
 
 var initController = require('./controllers/mainController');
 
