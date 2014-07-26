29213a0cf4a43cbecbef448503395b998e1a9cb8
opensas <opensas@gmail.com>
2014-07-26 03:59:00 -0300

    agregamos grunt-nodemon

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,15 +15,25 @@ module.exports = function(grunt) {
     watch: {
       files: ['./*.js', './**/*.js', 'lib/**/*.js', 'test/**/*.js'],
       tasks: ['test']
+    },
+
+    nodemon: {
+      dev: {
+        script: 'app.js'
+      }
     }
 
   });
 
   grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-nodemon');
+
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('test', 'mochaTest');
 
-  grunt.registerTask('default', 'mochaTest');
+  grunt.registerTask('exec', 'nodemon');
+
+  grunt.registerTask('default', 'exec');
 
 };
